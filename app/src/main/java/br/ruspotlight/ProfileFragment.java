571e7b1616d064a7ecf613184b461a8808a6b779
Kaio Max <kaiomaks@gmail.com>
2016-10-21 23:42:02 -0300

    Add ProfileFragment content base

--- app/src/main/java/br/ruspotlight/ProfileFragment.java
+++ app/src/main/java/br/ruspotlight/ProfileFragment.java
@@ -2,12 +2,23 @@ package br.ruspotlight;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.util.ArrayList;
+
+import br.ruspotlight.adapters.MealAdapter;
+import br.ruspotlight.domain.Meal;
+
 
 public class ProfileFragment extends Fragment {
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
+
     public ProfileFragment() {
         // Required empty public constructor
     }
@@ -20,8 +31,22 @@ public class ProfileFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+
+        View v =  inflater.inflate(R.layout.fragment_profile, container, false);
+
+        mRecyclerView = (RecyclerView) v.findViewById(R.id.recycler_view);
+        mRecyclerView.setHasFixedSize(true);
+
+        mLayoutManager = new LinearLayoutManager(getActivity());
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
+        ArrayList<Meal> list = new ArrayList<Meal>();
+        list.add(new Meal("Almoço", "01/10/2016"));
+
+        MealAdapter adapter = new MealAdapter(list, getActivity());
+        mRecyclerView.setAdapter(adapter);
+
+        return  v;
     }
 
 
