79c8f9e22dadc4af1cd0c15bcd99252c567605d5
carlosantq <carlosantonio.o.n@outlook.com>
2016-10-25 22:39:04 -0300

    MealActivity Content and Rating FAB

--- app/src/main/java/br/ruspotlight/MealActivity.java
+++ app/src/main/java/br/ruspotlight/MealActivity.java
@@ -1,13 +1,22 @@
 package br.ruspotlight;
 
+import android.content.res.ColorStateList;
+import android.media.Rating;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.ViewStub;
+import android.widget.RatingBar;
 
 public class MealActivity extends AppCompatActivity {
+
+    private boolean checked;
+    private FloatingActionButton fab;
+    private View inflated;
+    private ViewStub stub;
+    private RatingBar mealRating;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,12 +26,46 @@ public class MealActivity extends AppCompatActivity {
         toolbar.setTitle(getIntent().getStringExtra("TITLE"));
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setBackgroundTintList(ColorStateList.valueOf(0xFFef625b));
+        checked = false;
+
+        stub = (ViewStub) findViewById(R.id.stub_ranking);
+        inflated = stub.inflate();
+        inflated.setVisibility(View.GONE);
+
+        mealRating = (RatingBar) findViewById(R.id.rating_bar);
+
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                ViewStub stub = (ViewStub) findViewById(R.id.stub_ranking);
-                View inflated = stub.inflate();
+                if (!checked){
+
+                    checked = true;
+                    inflated.setVisibility(View.VISIBLE);
+                }else{
+                    inflated.setVisibility(View.GONE);
+                    checked = false;
+                }
+
+            }
+        });
+
+        /*
+        mealRating.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                mealRating.setFocusable(false);
+            }
+        });*/
+
+
+        mealRating.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
+            @Override
+            public void onRatingChanged(RatingBar ratingBar, float v, boolean b) {
+                fab.setBackgroundTintList(ColorStateList.valueOf(0xFF4CAF50));
+                mealRating.setClickable(false);
+                mealRating.setFocusable(false);
             }
         });
     }
