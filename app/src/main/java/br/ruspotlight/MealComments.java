f5be3b3c3a7d08f6550bd3517e3473778229dad0
Rafael <rafaom260@yahoo.com.br>
2016-11-14 14:22:28 -0300

    Uso do padrão composite para a classe Comment

--- app/src/main/java/br/ruspotlight/MealComments.java
+++ app/src/main/java/br/ruspotlight/MealComments.java
@@ -4,20 +4,19 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-import java.util.ArrayList;
 import java.util.List;
 
-import br.ruspotlight.domain.Comment;
+import br.ruspotlight.domain.CommentItem;
 
 public class MealComments extends AppCompatActivity {
 
-    private List<Comment> comments;
+    private List<CommentItem> commentItems;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_meal_comments);
         //Intent para receber a lista de comentarios da refeicao selecionada
         Intent intentReceived = getIntent();
-        //comments = (ArrayList<Comment>) intentReceived.getExtras().get("comments");
+        //commentItems = (ArrayList<CommentItem>) intentReceived.getExtras().get("commentItems");
     }
 }
