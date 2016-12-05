d87befb5e62f1507207dfe95a6c8bca2a94389e2
Kaio Max <kaiomaks@gmail.com>
2016-12-05 13:13:56 -0300

    Add Ratings to Meals

--- app/src/main/java/br/ruspotlight/HomeFragment.java
+++ app/src/main/java/br/ruspotlight/HomeFragment.java
@@ -6,7 +6,6 @@ import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,7 +13,6 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.amulyakhare.textdrawable.TextDrawable;
-import com.amulyakhare.textdrawable.util.ColorGenerator;
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
@@ -25,8 +23,6 @@ public class HomeFragment extends Fragment {
     private RecyclerView mealsList;
 
     private DatabaseReference mDatabaseMeals;
-
-    private ColorGenerator colorGenerator = ColorGenerator.MATERIAL;
 
     public HomeFragment() {
         // Required empty public constructor
--- app/src/main/java/br/ruspotlight/MealActivity.java
+++ app/src/main/java/br/ruspotlight/MealActivity.java
@@ -1,43 +1,53 @@
 package br.ruspotlight;
 
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.res.ColorStateList;
-//import android.media.Rating;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewStub;
 import android.widget.RatingBar;
+import android.widget.Toast;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-
-import br.ruspotlight.domain.Rating;
+import com.google.firebase.database.ValueEventListener;
 
 public class MealActivity extends AppCompatActivity {
 
-    private boolean checkedFab;
+    private static final String TAG = "RUS/MEAL";
+
+    private String mealKey = null;
+    private boolean checkedFab = false;
     private FloatingActionButton fab;
     private View inflated;
     private ViewStub stub;
     private RatingBar mealRating;
+
     private DatabaseReference mDatabase;
+    private FirebaseUser user;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_meal);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        toolbar.setTitle(getIntent().getStringExtra("TITLE"));
+        toolbar.setTitle(getIntent().getStringExtra("title"));
         setSupportActionBar(toolbar);
+
+        mealKey = getIntent().getExtras().getString("meal_key");
+        mDatabase = FirebaseDatabase.getInstance().getReference().child("meals").child(mealKey);
+        user = FirebaseAuth.getInstance().getCurrentUser();
 
         fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setBackgroundTintList(ColorStateList.valueOf(0xFF8A8E9C));
-        checkedFab = false;
 
         stub = (ViewStub) findViewById(R.id.stub_ranking);
         inflated = stub.inflate();
@@ -45,19 +55,23 @@ public class MealActivity extends AppCompatActivity {
 
         mealRating = (RatingBar) findViewById(R.id.rating_bar);
 
-        mDatabase = FirebaseDatabase.getInstance().getReference().child("rates");
+        checkUserRated();
 
         fab.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view){
-                if (!checkedFab) {
-                    fab.setImageResource(R.drawable.check);
-                    fab.setBackgroundTintList(ColorStateList.valueOf(0xFF4CAF50));
-                    checkedFab = true;
-                    inflated.setVisibility(View.VISIBLE);
-                }else{
-                    inflated.setVisibility(View.GONE);
-                    checkedFab = false;
+                if(user == null) {
+                    Toast.makeText(MealActivity.this, "É necessário realizar o login para poder avaliar.", Toast.LENGTH_LONG).show();
+                } else {
+                    if (!checkedFab) {
+                        fab.setImageResource(R.drawable.check);
+                        fab.setBackgroundTintList(ColorStateList.valueOf(0xFF4CAF50));
+                        checkedFab = true;
+                        inflated.setVisibility(View.VISIBLE);
+                    }else{
+                        inflated.setVisibility(View.GONE);
+                        checkedFab = false;
+                    }
                 }
             }
         });
@@ -65,35 +79,44 @@ public class MealActivity extends AppCompatActivity {
         mealRating.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
             @Override
             public void onRatingChanged(RatingBar ratingBar, float v, boolean b) {
-                System.out.println("Rating: " + mealRating.getRating());
+                Log.d(TAG, Float.toString(mealRating.getRating()));
                 mealRating.setClickable(false);
                 mealRating.setFocusable(false);
 
-                new AlertDialog.Builder(MealActivity.this)
-                        .setIcon(R.mipmap.warning)
-                        .setTitle("Confirmar Avaliação")
-                        .setMessage("Esta é a sua nota final?")
-                        .setPositiveButton("Sim", new DialogInterface.OnClickListener()
-                        {
-                            @Override
-                            public void onClick(DialogInterface dialog, int which) {
-                                mealRating.setIsIndicator(true);
-                                mealRating.setEnabled(false);
-
-                                DatabaseReference newRateRef = mDatabase.push();
-                                //newRateRef.setValue(new Rating((double) mealRating.getRating(), user));
-                                newRateRef.setValue(new Rating((double) mealRating.getRating()));
-                            }
-
-                        })
-                        .setNegativeButton("Não", null)
-                        .show();
+                DatabaseReference ratesRef = mDatabase.child("ratings");
+                ratesRef.child(user.getUid()).setValue(mealRating.getRating());
             }
         });
     }
 
-    public void verComentarios(View view) {
+    public void showComments(View view) {
         Intent i = new Intent(MealActivity.this, CommentsActivity.class);
         startActivity(i);
+    }
+
+    private void checkUserRated() {
+        if(user != null) {
+            final String userId = user.getUid();
+
+            mDatabase.child("ratings").addValueEventListener(new ValueEventListener() {
+                @Override
+                public void onDataChange(DataSnapshot dataSnapshot) {
+                    DataSnapshot userRatingSnapshot = dataSnapshot.child(userId);
+                    if(userRatingSnapshot.getValue() != null) {
+                        Float rating = userRatingSnapshot.getValue(Float.class);
+
+                        fab.setImageResource(R.drawable.check);
+                        fab.setBackgroundTintList(ColorStateList.valueOf(0xFF4CAF50));
+                        checkedFab = true;
+                        inflated.setVisibility(View.VISIBLE);
+                        mealRating.setRating(rating);
+                    }
+                }
+
+                @Override
+                public void onCancelled(DatabaseError databaseError) {
+                }
+            });
+        }
     }
 }
