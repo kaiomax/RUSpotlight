b3e1e18e1e9ae958840bc8dcf159becb1e87a8b4
Kaio Max <kaiomaks@gmail.com>
2016-12-07 00:06:16 -0300

    Set Meal chart data

--- app/src/main/java/br/ruspotlight/StatisticsFragment.java
+++ app/src/main/java/br/ruspotlight/StatisticsFragment.java
@@ -2,6 +2,7 @@ package br.ruspotlight;
 
 import android.app.DatePickerDialog;
 import android.graphics.Color;
+import android.graphics.Paint;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -9,7 +10,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.DatePicker;
-import android.widget.RadioButton;
 import android.widget.TextView;
 
 import com.github.mikephil.charting.charts.PieChart;
@@ -17,20 +17,27 @@ import com.github.mikephil.charting.data.Entry;
 import com.github.mikephil.charting.data.PieData;
 import com.github.mikephil.charting.data.PieDataSet;
 import com.github.mikephil.charting.formatter.PercentFormatter;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.GenericTypeIndicator;
+import com.google.firebase.database.ValueEventListener;
 
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Calendar;
-import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import static android.support.v7.content.res.AppCompatResources.getDrawable;
 
 
 public class StatisticsFragment extends Fragment {
 
-    private PieChart chart;
+    private PieChart lunchChart, dinnerChart;
     private TextView dateView;
     private Button dayBtn;
     private Button weekBtn;
@@ -38,7 +45,6 @@ public class StatisticsFragment extends Fragment {
 
     private static final String[] xData = {"★", "★★", "★★★", "★★★★", "★★★★★"};
     private Calendar calendar = Calendar.getInstance();
-    private int radioIdChecked = R.id.radioTodos; // id do radio atualmente ativo
     private int buttonIdChecked = R.id.dayBtn; // id do botao atualmente ativo (Dia, Semana ou Mes)
 
 
@@ -56,23 +62,22 @@ public class StatisticsFragment extends Fragment {
                              Bundle savedInstanceState) {
 
         View statisticsView = inflater.inflate(R.layout.fragment_statistics, container, false);
-        chart = (PieChart)statisticsView.findViewById(R.id.chart);
-        dateView = (TextView)statisticsView.findViewById(R.id.dateView);
-        dayBtn = (Button)statisticsView.findViewById(R.id.dayBtn);
-        weekBtn = (Button)statisticsView.findViewById(R.id.weekBtn);
-        monthBtn = (Button)statisticsView.findViewById(R.id.monthBtn);
+
+        lunchChart = (PieChart) statisticsView.findViewById(R.id.chart_lunch);
+        setChartOptions(lunchChart);
+
+        dinnerChart = (PieChart) statisticsView.findViewById(R.id.chart_dinner);
+        setChartOptions(dinnerChart);
+
+        dateView = (TextView) statisticsView.findViewById(R.id.dateView);
+        dayBtn = (Button) statisticsView.findViewById(R.id.dayBtn);
+        weekBtn = (Button) statisticsView.findViewById(R.id.weekBtn);
+        monthBtn = (Button) statisticsView.findViewById(R.id.monthBtn);
 
         View.OnClickListener onPreviousOrNextDayClickedListener = new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 onPreviousOrNextDayClicked(v);
-            }
-        };
-
-        View.OnClickListener onRadioButtonClickedListener = new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                onRadioButtonClicked(v);
             }
         };
 
@@ -86,12 +91,6 @@ public class StatisticsFragment extends Fragment {
         statisticsView.findViewById(R.id.previousDay).setOnClickListener(onPreviousOrNextDayClickedListener);
 
         statisticsView.findViewById(R.id.nextDay).setOnClickListener(onPreviousOrNextDayClickedListener);
-
-        statisticsView.findViewById(R.id.radioAlmoco).setOnClickListener(onRadioButtonClickedListener);
-
-        statisticsView.findViewById(R.id.radioJantar).setOnClickListener(onRadioButtonClickedListener);
-
-        statisticsView.findViewById(R.id.radioTodos).setOnClickListener(onRadioButtonClickedListener);
 
         statisticsView.findViewById(R.id.dayBtn).setOnClickListener(onDayWeekOrMonthButtonClickedListener);
 
@@ -121,79 +120,20 @@ public class StatisticsFragment extends Fragment {
             }
         });
 
-
         return statisticsView;
     }
 
     @Override
     public void onActivityCreated(Bundle t) {
         super.onActivityCreated(t);
-        setupChart();
         updateView();
     }
 
-    // Os dados a pegar do firebase (Quantos votaram para cada numero de estrelas)
-    // indice 0 -> Quantos votaram 1 estrela
-    // indice 1 -> Quantos votaram 2 estrelas
-    // ...
-    // indice 4 -> Quantos votaram 5 estrelas
-    // Use os campos radioIdChecked e  buttonIdChecked para determinar os filtros
-    private List<Float> getYData() {
-        List result = new ArrayList();
-        Date from = calendar.getTime();
-        Date to = calendar.getTime();
-
-        switch (buttonIdChecked) {
-            case R.id.dayBtn: // pega apenas os votos do dia
-                break;
-
-            case R.id.weekBtn: { // pega apenas os votos na semana
-                Calendar cal = Calendar.getInstance();
-                cal.setFirstDayOfWeek(Calendar.MONDAY);
-                cal.setTime(calendar.getTime());
-                cal.set(Calendar.WEEK_OF_YEAR, calendar.get(Calendar.WEEK_OF_YEAR));
-                cal.set(Calendar.DAY_OF_WEEK, Calendar.MONDAY);
-                from = cal.getTime();
-                cal.set(Calendar.DAY_OF_WEEK, Calendar.SUNDAY);
-                to = cal.getTime();
-                break;
-            }
-            case R.id.monthBtn: { // pega todos os votos do mes
-                Calendar cal = Calendar.getInstance();
-                cal.setTime(calendar.getTime());
-                cal.set(Calendar.DAY_OF_MONTH, 1);
-                from = cal.getTime();
-                cal.set(Calendar.DAY_OF_MONTH, cal.getActualMaximum(Calendar.DAY_OF_MONTH));
-                to = cal.getTime();
-                break;
-            }
-        }
-
-
-        // TODO: Aki deve ficar o codigo  que filtra por data. Use a variavel 'from' para a data minima
-        // e a variavel 'to' para a data maxima
-
-
-        switch (radioIdChecked) {
-            case R.id.radioAlmoco: {
-                // TODO: Ponha aki o codigo para filtrar apenas para almoco
-                break;
-            }
-
-            case R.id.radioJantar: {
-                // TODO: Ponha aki o codigo para filtrar para apenas jantar
-                break;
-            }
-        }
-
-        result = Arrays.asList((float) 10, (float) 20, (float) 30.6, (float) 20.1, (float) 19.3); // Exemplo para testes!
-
-        return result;
-    }
-
-    // Define as configuracoes iniciasi do grafico
-    private void setupChart() {
-        chart = (PieChart)getView().findViewById(R.id.chart);
+    private void setChartOptions(PieChart chart) {
+        Paint p = chart.getPaint(PieChart.PAINT_INFO);
+        p.setTextSize(32);
+        p.setColor(0xFF444444);
+        chart.setNoDataText("Dados indisponíveis.");
         chart.setUsePercentValues(true);
         chart.setHoleColor(Color.TRANSPARENT);
         chart.setHoleRadius(40f);
@@ -205,12 +145,61 @@ public class StatisticsFragment extends Fragment {
         chart.getLegend().setEnabled(false);
     }
 
-    // Atualiza os dados do gráfico
-    private void updateGraphData(List<Float> yData) {
-        ArrayList<Entry> yVals1 = new ArrayList<>();
+    private List<Integer> calculateRatings(Map<String, Integer> ratings) {
+        Map<Integer, Integer> ratingsMap = new HashMap<Integer, Integer>();
+        ratingsMap.put(1, 0);
+        ratingsMap.put(2, 0);
+        ratingsMap.put(3, 0);
+        ratingsMap.put(4, 0);
+        ratingsMap.put(5, 0);
+
+        for(Integer rating : ratings.values()) {
+            ratingsMap.put(rating, ratingsMap.get(rating) + 1);
+        }
+
+        return Arrays.asList(
+                ratingsMap.get(1),
+                ratingsMap.get(2),
+                ratingsMap.get(3),
+                ratingsMap.get(4),
+                ratingsMap.get(5));
+    }
+
+    private void updateChartReference(final String mealKey, final PieChart chart) {
+        DatabaseReference mDatabase = FirebaseDatabase.getInstance().getReference().child("meals").child(mealKey);
+
+        mDatabase.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                GenericTypeIndicator<Map<String, Integer>> t = new GenericTypeIndicator<Map<String, Integer>>() {};
+                Map<String, Integer> ratings = dataSnapshot.child("ratings").getValue(t);
+                if( ratings == null ) {
+                    clearChartData(chart);
+                }
+                else {
+                    setChartData(chart, calculateRatings(ratings));
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+    private void clearChartData(PieChart chart) {
+        chart.clear();
+        chart.setData(null);
+        chart.highlightValues(null);
+        chart.invalidate();
+    }
+
+    private void setChartData(PieChart chart, List<Integer> yData) {
+        ArrayList<Entry> yVals = new ArrayList<>();
 
         for (int i = 0; i < yData.size(); ++i) {
-            yVals1.add(new Entry(yData.get(i), i));
+            yVals.add(new Entry(yData.get(i), i));
         }
 
         ArrayList<String> xVals = new ArrayList<>();
@@ -219,17 +208,17 @@ public class StatisticsFragment extends Fragment {
             xVals.add(xData[i]);
         }
 
-        PieDataSet dataSet = new PieDataSet(yVals1, "");
+        PieDataSet dataSet = new PieDataSet(yVals, "");
         dataSet.setSliceSpace(3);
         dataSet.setSelectionShift(5);
 
         ArrayList<Integer> colors = new ArrayList<>();
 
-        colors.add(0xFFFF0000);
-        colors.add(0xFFFFA500);
-        colors.add(0xFFFFFF00);
-        colors.add(0xFF9ACD32);
-        colors.add(0xFF00FF00);
+        colors.add(0xFFDD2C00);
+        colors.add(0xFFFF6D00);
+        colors.add(0xFFFFAB00);
+        colors.add(0xFFAEEA00);
+        colors.add(0xFF64DD17);
 
         dataSet.setColors(colors);
 
@@ -279,32 +268,6 @@ public class StatisticsFragment extends Fragment {
         updateView();
     }
 
-
-    // Ao clicar nos radioButton
-    public void onRadioButtonClicked(View view) {
-        // Is the button now checked?
-        boolean checked = ((RadioButton) view).isChecked();
-
-        // Check which radio button was clicked
-        switch (view.getId()) {
-            case R.id.radioAlmoco:
-                if (checked)
-                    radioIdChecked = R.id.radioAlmoco;
-                break;
-            case R.id.radioJantar:
-                if (checked)
-                    radioIdChecked = R.id.radioJantar;
-                break;
-            case R.id.radioTodos:
-                if (checked)
-                    radioIdChecked = R.id.radioTodos;
-                break;
-        }
-
-        updateView();
-    }
-
-    // Atualiza toda a view, nao apenas o grafico
     private void updateView() {
         SimpleDateFormat sdf;
         Calendar cal = Calendar.getInstance();
@@ -319,6 +282,10 @@ public class StatisticsFragment extends Fragment {
             case R.id.dayBtn:
                 sdf = new SimpleDateFormat("dd/MM/yyyy");
                 dateView.setText(sdf.format(calendar.getTime()));
+                sdf = new SimpleDateFormat("dd-MM-yy");
+                String date = sdf.format(calendar.getTime());
+                updateChartReference(date + "-A", lunchChart);
+                updateChartReference(date + "-J", dinnerChart);
                 break;
 
             case R.id.weekBtn:
@@ -339,7 +306,47 @@ public class StatisticsFragment extends Fragment {
                 dateView.setText(sdf.format(cal.getTime()));
                 break;
         }
-
-        updateGraphData(getYData()); // Atualiza o grafico
     }
+
+    // Os dados a pegar do firebase (Quantos votaram para cada numero de estrelas)
+    // indice 0 -> Quantos votaram 1 estrela
+    // indice 1 -> Quantos votaram 2 estrelas
+    // ...
+    // indice 4 -> Quantos votaram 5 estrelas
+    // Use os campos radioIdChecked e  buttonIdChecked para determinar os filtros
+    /*private List<Float> getYData() {
+        List result = new ArrayList();
+        Date from = calendar.getTime();
+        Date to = calendar.getTime();
+
+        switch (buttonIdChecked) {
+            case R.id.dayBtn: // pega apenas os votos do dia
+                break;
+
+            case R.id.weekBtn: { // pega apenas os votos na semana
+                Calendar cal = Calendar.getInstance();
+                cal.setFirstDayOfWeek(Calendar.MONDAY);
+                cal.setTime(calendar.getTime());
+                cal.set(Calendar.WEEK_OF_YEAR, calendar.get(Calendar.WEEK_OF_YEAR));
+                cal.set(Calendar.DAY_OF_WEEK, Calendar.MONDAY);
+                from = cal.getTime();
+                cal.set(Calendar.DAY_OF_WEEK, Calendar.SUNDAY);
+                to = cal.getTime();
+                break;
+            }
+            case R.id.monthBtn: { // pega todos os votos do mes
+                Calendar cal = Calendar.getInstance();
+                cal.setTime(calendar.getTime());
+                cal.set(Calendar.DAY_OF_MONTH, 1);
+                from = cal.getTime();
+                cal.set(Calendar.DAY_OF_MONTH, cal.getActualMaximum(Calendar.DAY_OF_MONTH));
+                to = cal.getTime();
+                break;
+            }
+        }
+
+        result = Arrays.asList((float) 10, (float) 9, (float) 28, (float) 21, (float) 32); // Exemplo para testes!
+
+        return result;
+    }*/
 }
